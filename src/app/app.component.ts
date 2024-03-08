b018fce78659ab99301e93ba379eff97fa71d7e7
Francisco Castro <francisco.pessanha@ibm.com>
2024-03-08 09:36:35 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AccountService } from './account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'services-ACG';
+export class AppComponent implements OnInit, OnChanges {
+  accounts: {name:string, status: string}[] = [];
+
+  constructor(private accountService: AccountService) {
+    
+  }
+  
+  ngOnInit(): void {
+    this.accounts = this.accountService.accounts;
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    
+  }
+ 
 }
